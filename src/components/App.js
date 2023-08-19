e1aaaca800b949314a0cc066a5f0ddf58fe83448
Mathieu Joyal <mathieujoyal96@gmail.com>
2023-08-19 11:46:24 -0400

    added a line or two

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>My Portfolio Project</h1>
+      <section>
+        <h2>About Me</h2>
+        <p>I'm a big nerd</p>
+      </section>
     </div>
   );
 }
